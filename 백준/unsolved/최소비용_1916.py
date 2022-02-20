e812be03b91fac46e00d135d77bc3dac678fc578
jay <jaewhan232@naver.com>
2022-02-20 20:10:35 +0900

    다익스트라

---
+++ 백준/unsolved/최소비용_1916.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+
+input = stdin.readline
+
+N = int(input())
+M = int(input())
+lines = [[] for _ in range(N+1)]
+
+
+for _ in range(M):
+    sp,ep,w = map(int,input().split())
+    
+
+heap = []
+def djk(start):
+    
